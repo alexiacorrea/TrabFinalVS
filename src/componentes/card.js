952a5190b5bbbdb3e03f1d8ccb0d68661192ec91
alexiacorrea <105754595+alexiacorrea@users.noreply.github.com>
2023-12-14 20:41:47 -0300

    Update card.js

--- src/componentes/card.js
+++ src/componentes/card.js
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom"
 
 export default function Card(props) {
+
     function excluir(){
     fetch(`http://localhost:8080/tarefas/${props.tarefa.id}`,
       {
@@ -8,27 +9,30 @@ export default function Card(props) {
       })
       .then(x => alert('Tarefa excluída com sucesso!'))
     }
+
     return (
-        <div className="card mt-3 col-sm-3">
+        <div className="card text-bg-dark mb-3">
             <div className="card-body">
                 <div>
                     <span className="fw-bold me-1">Título:</span>
-                    <span className="text-danger">{props.tarefa.titulo}</span>
+                    <span className=".text-white">{props.tarefa.titulo}</span>
                 </div>
                 <div>
                     <span className="fw-bold me-1">Data:</span>
-                    <span className="text-danger">{props.tarefa.data}</span>
+                    <span className=".text-white">{props.tarefa.data}</span>
                 </div>
                 <div>
                     <span className="fw-bold me-1">Conteudo:</span>
-                    <span className="text-danger">{props.tarefa.conteudo}</span>
+                    <span className=".text-white">{props.tarefa.conteudo}</span>
                 </div>
                 <div>
                     <span className="fw-bold me-1">Status:</span>
-                    <span className="text-danger">{props.tarefa.status}</span>
+                    <span className=".text-white">{props.tarefa.status}</span>
                 </div>
                 <div className="mt-3 d-flex justify-content-between">
-                    <Link to={`/tarefa/editar/${props.tarefa.id}`}><button className="btn btn-outline-primary">Editar</button></Link>
+                <Link to={`/tarefas/editar/${props.tarefa.id}`}>
+                    <button className="btn btn-outline-primary">Editar</button>
+                </Link>
                     <button onClick={excluir} className="btn btn-outline-danger" >Excluir</button>
                 </div>
             </div>
