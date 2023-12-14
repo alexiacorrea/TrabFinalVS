626f0af5b6c8fe4c6f78083124dfa28d8a1ad854
alexiacorrea <105754595+alexiacorrea@users.noreply.github.com>
2023-12-14 20:42:12 -0300

    Update consulta.js

--- src/componentes/consulta.js
+++ src/componentes/consulta.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react"
 import { useState } from "react"
 import Card from "./card"
+import { Link } from 'react-router-dom';
 
 export default function Consulta() {
     const [tarefas, setTarefas] = useState([])
@@ -15,12 +16,16 @@ export default function Consulta() {
 
     return (
         <div>
-            <h2>Consulta de tarefas</h2>
+            <Link to="/">
+                <button className="btn" data-bs-toggle="button">Voltar para página inicial</button>
+            </Link>
+            <h2 className="text-center">Consulta de tarefas</h2>
+            
             <div className="d-flex flex-wrap">
                 {
                     tarefas.map(tf => <Card tarefa={tf} />)
                 }
             </div> 
-        </div>
+            </div>
     )
 }
