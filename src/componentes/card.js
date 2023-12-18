af04673a77af376b5e290ee6222fd214c6893198
alexiacorrea <105754595+alexiacorrea@users.noreply.github.com>
2023-12-18 10:01:54 -0300

    Update card.js

--- src/componentes/card.js
+++ src/componentes/card.js
@@ -1,39 +1,37 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 export default function Card(props) {
-
-    function excluir(){
-    fetch(`http://localhost:8080/tarefas/${props.tarefa.id}`,
-      {
-        method:'DELETE'        
-      })
-      .then(x => alert('Tarefa excluída com sucesso!'))
+    function excluir() {
+        fetch(`http://localhost:8080/tarefas/${props.tarefa.id}`, {
+            method: 'DELETE'
+        })
+        .then(x => alert('Tarefa excluída com sucesso!'))
     }
 
     return (
-        <div className="card text-bg-dark mb-3">
-            <div className="card-body">
-                <div>
+        <div className="card bg-warning-subtle mb-3" style={{ maxWidth: '18rem' }}>
+            <div className="card-body d-flex flex-column">
+                <div className="mb-2">
                     <span className="fw-bold me-1">Título:</span>
-                    <span className=".text-white">{props.tarefa.titulo}</span>
+                    <span>{props.tarefa.titulo}</span>
                 </div>
-                <div>
+                <div className="mb-2">
                     <span className="fw-bold me-1">Data:</span>
-                    <span className=".text-white">{props.tarefa.data}</span>
+                    <span>{props.tarefa.data}</span>
                 </div>
-                <div>
-                    <span className="fw-bold me-1">Conteudo:</span>
-                    <span className=".text-white">{props.tarefa.conteudo}</span>
+                <div className="mb-2">
+                    <span className="fw-bold me-1">Conteúdo:</span>
+                    <span>{props.tarefa.conteudo}</span>
                 </div>
-                <div>
+                <div className="mb-2">
                     <span className="fw-bold me-1">Status:</span>
-                    <span className=".text-white">{props.tarefa.status}</span>
+                    <span>{props.tarefa.status}</span>
                 </div>
-                <div className="mt-3 d-flex justify-content-between">
-                <Link to={`/tarefas/editar/${props.tarefa.id}`}>
-                    <button className="btn btn-outline-primary">Editar</button>
-                </Link>
-                    <button onClick={excluir} className="btn btn-outline-danger" >Excluir</button>
+                <div className="mt-auto d-flex justify-content-between">
+                    <Link to={`/tarefas/editar/${props.tarefa.id}`}>
+                        <button className="btn btn-warning">Editar</button>
+                    </Link>
+                    <button onClick={excluir} className="btn btn-danger">Excluir</button>
                 </div>
             </div>
         </div>
