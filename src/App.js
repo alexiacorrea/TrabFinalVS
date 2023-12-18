891e661dbecb8aecaca8b0144caf9085a3886f81
alexiacorrea <105754595+alexiacorrea@users.noreply.github.com>
2023-12-18 09:59:35 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
-import {Link, Outlet } from 'react-router-dom'
+import { Link, Outlet } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Tarefas</h1>
-      <nav>
-        <Link to='/tarefas/cadastrar'>Cadastro</Link> &nbsp;
-        <Link to='/tarefas/consultar'>Consulta</Link> &nbsp;
-      </nav>
-      <main>
-         <Outlet />
-      </main>
+    <div className="container-fluid d-flex justify-content-center align-items-center vh-100" style={{ backgroundColor: '#f2f2f2' }}>
+      <div className="card text-center p-4" style={{ backgroundColor: '#fff', boxShadow: '0px 0px 10px 0px rgba(0,0,0,0.1)' }}>
+        <div className="mb-4">
+          <a className="navbar-brand" href="/">
+            <img src="/sticky-notes.png" alt="Notebook" width="40" height="40" className="d-inline-block align-top" />
+            <span className="ms-2 fs-4 fw-bold">Tarefas</span>
+          </a>
+        </div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light mb-4 justify-content-center">
+          <div className="collapse navbar-collapse justify-content-center">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link className="nav-link" to='/tarefas/cadastrar'>Cadastro</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to='/tarefas/consultar'>Consulta</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <main>
+          <Outlet />
+        </main>
+      </div>
     </div>
   );
 }
