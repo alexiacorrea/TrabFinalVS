b7ef2a3995e977084fb2bcc2237e8366ddc1d49e
alexiacorrea <105754595+alexiacorrea@users.noreply.github.com>
2023-12-18 10:02:39 -0300

    Update consulta.js

--- src/componentes/consulta.js
+++ src/componentes/consulta.js
@@ -1,31 +1,37 @@
-import { useEffect } from "react"
-import { useState } from "react"
-import Card from "./card"
-import { Link } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import Card from "./card";
+import { Link } from "react-router-dom";
 
 export default function Consulta() {
-    const [tarefas, setTarefas] = useState([])
-    function consultar() {
-        fetch('http://localhost:8080/tarefas')
-            .then(data => data.json())
-            .then(response => setTarefas(response))
-    }
-    
+    const [tarefas, setTarefas] = useState([]);
 
-    useEffect(consultar, [])
+    function consultar() {
+        fetch("http://localhost:8080/tarefas")
+            .then((data) => data.json())
+            .then((response) => setTarefas(response));
+    }
+
+    useEffect(consultar, []);
 
     return (
-        <div>
+        <div className="container">
             <Link to="/">
-                <button className="btn" data-bs-toggle="button">Voltar para página inicial</button>
+                <button className="btn" data-bs-toggle="button">
+                    Voltar para página inicial
+                </button>
             </Link>
             <h2 className="text-center">Consulta de tarefas</h2>
-            
-            <div className="d-flex flex-wrap">
-                {
-                    tarefas.map(tf => <Card tarefa={tf} />)
-                }
-            </div> 
+            <div className="card bg-light">
+                <div className="card-body">
+                    <div className="row row-cols-1 row-cols-md-3 g-4">
+                        {tarefas.map((tf) => (
+                            <div className="col" key={tf.id}>
+                                <Card tarefa={tf} />
+                            </div>
+                        ))}
+                    </div>
+                </div>
             </div>
-    )
+        </div>
+    );
 }
